c6dcffe05c7952367700df228433d62a70945e6e
Lauren <lcc4935@rit.edu>
2021-10-29 02:14:00 -0400

    B

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -11,6 +11,7 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
+  req.sesion.destroy();
   res.redirect('/');
 };
 
@@ -30,6 +31,7 @@ const login = (request, response) => {
     if (err || !account) {
       return res.statue(401).json({ error: 'Wrong username or password' });
     }
+    req.session.account = Account.AccountModel.toAPI(account);
     return res.json({ redirect: '/maker' });
   });
 };
@@ -62,7 +64,10 @@ const signup = (request, response) => {
 
     const savePromise = newAccount.save();
 
-    savePromise.then(() => res.json({ redirect: '/maker' }));
+    savePromise.then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      res.json({ redirect: '/maker' });
+    });
 
     savePromise.catch((err) => {
       console.log(err);
